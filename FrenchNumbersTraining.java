ec3d7d0dae65cba7597f6399a6eaa8704fad8769
Ihor Zaiets <dekstroer8202@gmail.com>
2025-11-24 10:41:31 +0100

    fix: add imports

--- FrenchNumbersTraining.java
+++ FrenchNumbersTraining.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class FrenchNumbersTraining {
     public static void main(String[] args) {
